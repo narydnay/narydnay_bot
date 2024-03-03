11008f04a2fd66dfc211bf66a409333308e76276
Alekseyua <alekseyuadnepr@gmail.com>
2024-03-03 23:06:05 +0200

    add server

--- server/telegram/index.js
+++ server/telegram/index.js
@@ -4,12 +4,35 @@ import path, { dirname } from 'path';
 import { handlerActionUser } from "./actionButton/actionInlineKeyboard.js";
 import { matchNames, matchNamesSpecifics } from "../utils/util.js";
 import { listNames } from "./fakeDb/fakeDb.js";
+import express from "express";
+
+const config = {
+  token: '6884974307:AAEN0vj63vJ0ntxRoVSiqSnupPg3S2h7ymc',
+
+}
+
+const PORT = 3000;
+const app = express();
+
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
+const bot = new Telegraf(config.token);
+const tg = new Telegram(config.token);
 
-void async function () {
-  const bot = new Telegraf('6884974307:AAEN0vj63vJ0ntxRoVSiqSnupPg3S2h7ymc');
-  const tg = new Telegram('6884974307:AAEN0vj63vJ0ntxRoVSiqSnupPg3S2h7ymc');
+app.get('/',(req,res)=>{
+  res.send('Hi bro!!!')
+})
+app.post('/bot', (req,res)=>{
+  console.log(req);
+  const { body } = req;
+  bot.processUpdate(body)
+  res.status(200);
+})
+
+app.listen(PORT, ()=>console.log(`Server start on PORT ${PORT}`))
+
+
+
 
   bot.on('text', async ctx => {
     const { message } = ctx
@@ -65,15 +88,16 @@ void async function () {
 
 
   bot.launch({
-    webhook: {
-      domain: 'https://narydnay-bot.vercel.app/',// Your domain URL (where server code will be deployed)
-      port: 443
-    }
+    // webhook: {
+    //   domain: 'https://narydnay-bot.vercel.app/',// Your domain URL (where server code will be deployed)
+    //   port: 443
+    // }
+  }).then(() => {
+    console.info(`The bot ${bot.botInfo.username} is running on server`);
   });
 
   process.once('SIGINT', () => bot.stop('SIGINT'))
   process.once('SIGTERM', () => bot.stop('SIGTERM'))
 
-}()
 
 // ctx.replyWithPhoto({ source: img })
