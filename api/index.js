2b2d9e04988811a71e1765343cc5c03f1fe50cf9
Alekseyua <alekseyuadnepr@gmail.com>
2024-03-04 13:13:02 +0200

    create vercet json v9

--- api/index.js
+++ api/index.js
@@ -1,2 +1,14 @@
-import app from '../src/app.js';
+import express from "express";
+
+const PORT = 3000;
+const app = express();
+
+app.get('/',(req,res)=>{
+  console.log('all work')
+  res.send('Hi bro!!!')
+})
+
+app.listen(PORT, ()=>console.log(`Server start on PORT ${PORT}`))
+
+
 export default app;
