2a145366515c314fa40f7233175d3d0b70435cf2
Alekseyua <alekseyuadnepr@gmail.com>
2024-03-05 21:24:48 +0200

    try webhook started v 10

--- dist/index.js
+++ dist/index.js
@@ -4,8 +4,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const telegraf_1 = require("telegraf");
+const bot = new telegraf_1.Telegraf('6884974307:AAEN0vj63vJ0ntxRoVSiqSnupPg3S2h7ymc');
 const app = (0, express_1.default)();
 const PORT = 8000;
+bot.on('text', ctx => {
+    ctx.reply('hi bro we work good');
+});
 app.get('/', (_req, res) => {
     res.send('Router work good');
 });
