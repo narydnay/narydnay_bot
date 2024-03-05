c342a3662c6e6cb4350b8547c1c4fe2b1cd2d10f
Alekseyua <alekseyuadnepr@gmail.com>
2024-03-05 20:58:52 +0200

    try webhook started v 5

--- src/telegram/routers/botRouter.ts
+++ src/telegram/routers/botRouter.ts
@@ -1,6 +1,5 @@
 
-import { config } from "../../../config/server";
 import { routers } from "../../admin/controllers/admin";
 import { botController } from "../controllers/telegram";
 
-routers.post('/bot', botController.post )
+// routers.post('/bot', botController.post )
