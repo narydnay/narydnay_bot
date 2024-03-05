5fabbd6225a519b4b2f7c9820127eef0d10ec244
Alekseyua <alekseyuadnepr@gmail.com>
2024-03-05 21:45:28 +0200

    try webhook started v 12

--- dist/index.js
+++ dist/index.js
@@ -5,13 +5,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const telegraf_1 = require("telegraf");
-const bot = new telegraf_1.Telegraf('6884974307:AAEN0vj63vJ0ntxRoVSiqSnupPg3S2h7ymc');
+const TOKEN = '6884974307:AAEN0vj63vJ0ntxRoVSiqSnupPg3S2h7ymc';
+const WH_PATH = '/bot' + TOKEN;
+const bot = new telegraf_1.Telegraf(TOKEN);
 const app = (0, express_1.default)();
 const PORT = 8000;
+const URL_WEBHOOK = 'https://narydnay-bot.vercel.app/';
 bot.on('text', ctx => {
     ctx.reply('hi bro we work good');
 });
+bot.telegram.setWebhook(URL_WEBHOOK + WH_PATH);
 bot.launch();
+app.use(bot.webhookCallback(WH_PATH));
 app.get('/', (_req, res) => {
     res.send('Router work good');
 });
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,27 @@
 import express, { Request, Response} from 'express';
 import { Telegraf } from 'telegraf';
+const TOKEN = '6884974307:AAEN0vj63vJ0ntxRoVSiqSnupPg3S2h7ymc';
+const WH_PATH = '/bot' + TOKEN;
 
-const bot = new Telegraf('6884974307:AAEN0vj63vJ0ntxRoVSiqSnupPg3S2h7ymc');
+const bot = new Telegraf(TOKEN);
 
 const app = express();
 const PORT = 8000;
+const URL_WEBHOOK = 'https://narydnay-bot.vercel.app/';
 
 bot.on('text', ctx => {
   ctx.reply('hi bro we work good')
 })
+bot.telegram.setWebhook(URL_WEBHOOK + WH_PATH)
 bot.launch();
 
+app.use(bot.webhookCallback(WH_PATH))
 app.get('/', (_req: Request, res: Response)=>{
 
   res.send('Router work good')
 })
+
+
 
 app.listen(PORT, () => {
   console.log(`Server is listening on ${PORT} `);
