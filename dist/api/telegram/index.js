c00f2cf962c8b3f5b95740b59866edf5a232bfc9
Alekseyua <alekseyuadnepr@gmail.com>
2024-03-06 23:01:49 +0200

    new vision 9

--- dist/api/telegram/index.js
+++ dist/api/telegram/index.js
@@ -1,7 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const index_1 = require("../../index");
-index_1.bot.on('text', ctx => {
+const telegraf_1 = require("telegraf");
+const config_1 = require("../../config/config");
+const bot = new telegraf_1.Telegraf(config_1.config.get('token-bot'));
+bot.on('text', ctx => {
     ctx.reply('hi bro we work good ...? import data check');
 });
 //# sourceMappingURL=index.js.map
--- src/api/telegram/index.ts
+++ src/api/telegram/index.ts
@@ -1,4 +1,7 @@
-import { bot } from "../../index";
+import { Telegraf } from "telegraf";
+import { config } from "../../config/config";
+
+const bot = new Telegraf(config.get('token-bot'));
 
 bot.on('text', ctx => {
   ctx.reply('hi bro we work good ...? import data check')
