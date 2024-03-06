7e9c1de13b0c5730a6097faeb25af9300c637948
Alekseyua <alekseyuadnepr@gmail.com>
2024-03-07 00:16:18 +0200

    new vision 25

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,29 @@
 import express, { Request, Response} from 'express';
 import { Telegraf } from 'telegraf';
-// import { config } from './config/config';
-
 
 const TOKEN = '6884974307:AAEN0vj63vJ0ntxRoVSiqSnupPg3S2h7ymc';
-const URL_WEBHOOK = 'https://narydnay-bot.vercel.app';
-// const TOKEN = config.get('token-bot');
-// const URL_WEBHOOK = config.get('host');
 const WH_PATH = '/bot' + TOKEN;
 
-export const bot = new Telegraf(TOKEN);
+const bot = new Telegraf(TOKEN);
 
 const app = express();
 const PORT = 8000;
+const URL_WEBHOOK = 'https://narydnay-bot.vercel.app';
 
-// console.log({config: config.get('host')}) 
-bot.telegram.setWebhook(URL_WEBHOOK + WH_PATH)
+// Telegram API Configuration
+const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}`;
+const URI = `/webhook/${TOKEN}`;
+const webhookURL = `${URL_WEBHOOK}${URI}`;
+
 bot.on('text', ctx => {
-  ctx.reply('hi bro we work good ...? import data check')
+  ctx.reply('hi bro we work good')
 })
+bot.telegram.setWebhook('https://beige-worlds-matter.loca.lt' + WH_PATH)
 
 app.get('/', async (_req: Request, res: Response)=>{
    const dataBot = await bot.telegram.getMe()
-  res.send(JSON.stringify({deploy: 5,...dataBot}, null, 4 ))
-});
+  res.send(JSON.stringify(dataBot, null, 4))
+})
 
 app.use(bot.webhookCallback(WH_PATH))
 
@@ -33,7 +33,8 @@ app.listen(PORT, () => {
   } catch (error) {
     console.log(error.message);
   }
-});
+})
 
-process.once('SIGINT', () => bot.stop('SIGINT'));
-process.once('SIGTERM', () => bot.stop('SIGTERM'));
+
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
