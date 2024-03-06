ea9ed6974f3e4b4ead200eecbf2b47314a21ac7d
Alekseyua <alekseyuadnepr@gmail.com>
2024-03-06 23:51:38 +0200

    new vision 16

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,24 +1,24 @@
-interface Configuration {
-  get: (name: string) => void;
-}
+// interface Configuration {
+//   get: (name: string) => void;
+// }
 
-class Config implements Configuration {
+// class Config implements Configuration {
 
-  state = {
-    token_bot: '6884974307:AAEN0vj63vJ0ntxRoVSiqSnupPg3S2h7ymc',
-    host: 'https://narydnay-bot.vercel.app',
+//   state = {
+//     token_bot: '6884974307:AAEN0vj63vJ0ntxRoVSiqSnupPg3S2h7ymc',
+//     host: 'https://narydnay-bot.vercel.app',
 
-  }
+//   }
 
-  get(name: string) {
-    switch (name) {
-      case 'token-bot':
-        return this.state.token_bot;
-      case 'host':
-        return this.state.host;
-      default: return null;
-    }
-  }
-}
+//   get(name: string) {
+//     switch (name) {
+//       case 'token-bot':
+//         return this.state.token_bot;
+//       case 'host':
+//         return this.state.host;
+//       default: return null;
+//     }
+//   }
+// }
 
-export const config = new Config();
+// export const config = new Config();
