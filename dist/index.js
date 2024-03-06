bff3183879d5ac1e28645de1df6eeda4996b8140
Alekseyua <alekseyuadnepr@gmail.com>
2024-03-06 06:30:03 +0200

    new vision

--- dist/index.js
+++ dist/index.js
@@ -24,43 +24,23 @@ const URL_WEBHOOK = 'https://narydnay-bot.vercel.app';
 const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}`;
 const URI = `/webhook/${TOKEN}`;
 const webhookURL = `${URL_WEBHOOK}${URI}`;
-// // configuring the bot via Telegram API to use our route below as webhook
-// const setupWebhook = async () => {
-//   try {
-//     const { data } = await axios.get(
-//       `${TELEGRAM_API}/setWebhook?url=${webhookURL}&drop_pending_updates=true`
-//     );
-//     console.log(data);
-//   } catch (error) {
-//     return error;
-//   }
-// };
 bot.on('text', ctx => {
-    ctx.reply('hi bro we work good');
+    ctx.reply('hi bro we work good ...?');
 });
-bot.telegram.setWebhook(URL_WEBHOOK + WH_PATH);
-app.get('/', (_req, res) => {
-    res
-        .sendStatus(200)
-        .send('Router work good new data');
-});
-app.post(WH_PATH, (req, res) => {
-    const { body } = req;
-    // bot.webhookCallback(WH_PATH)
-    return res.sendStatus(200);
-});
+bot.telegram.setWebhook('https://narydnay-bot.vercel.app' + WH_PATH);
+app.get('/', (_req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const dataBot = yield bot.telegram.getMe();
+    res.send(JSON.stringify(dataBot, null, 4));
+}));
 app.use(bot.webhookCallback(WH_PATH));
-app.listen(PORT, () => __awaiter(void 0, void 0, void 0, function* () {
-    console.log(`Server is listening on ${PORT} `);
-    // setting up our webhook url on server spinup
+app.listen(PORT, () => {
     try {
         console.log(`Server is up and Running at PORT : ${PORT}`);
-        // await setupWebhook();
     }
     catch (error) {
         console.log(error.message);
     }
-}));
+});
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response} from 'express';
 import { Telegraf } from 'telegraf';
-import axios from 'axios';
+
 const TOKEN = '6884974307:AAEN0vj63vJ0ntxRoVSiqSnupPg3S2h7ymc';
 const WH_PATH = '/bot' + TOKEN;
 
@@ -15,44 +15,21 @@ const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}`;
 const URI = `/webhook/${TOKEN}`;
 const webhookURL = `${URL_WEBHOOK}${URI}`;
 
-// // configuring the bot via Telegram API to use our route below as webhook
-// const setupWebhook = async () => {
-//   try {
-//     const { data } = await axios.get(
-//       `${TELEGRAM_API}/setWebhook?url=${webhookURL}&drop_pending_updates=true`
-//     );
-//     console.log(data);
-//   } catch (error) {
-//     return error;
-//   }
-// };
-
 bot.on('text', ctx => {
-  ctx.reply('hi bro we work good')
+  ctx.reply('hi bro we work good ...?')
 })
-bot.telegram.setWebhook(URL_WEBHOOK + WH_PATH)
+bot.telegram.setWebhook('https://narydnay-bot.vercel.app' + WH_PATH)
 
-app.get('/', (_req: Request, res: Response)=>{
-
-  res
-  .sendStatus(200)
-  .send('Router work good new data')
-})
-
-app.post(WH_PATH, (req: Request, res: Response)=>{
-  const { body } = req;
-  // bot.webhookCallback(WH_PATH)
-  return res.sendStatus(200)
-})
+app.get('/', async (_req: Request, res: Response)=>{
+   const dataBot = await bot.telegram.getMe()
+  res.send(JSON.stringify(dataBot, null, 4  ))
+});
 
 app.use(bot.webhookCallback(WH_PATH))
 
-app.listen(PORT, async() => {
-  console.log(`Server is listening on ${PORT} `);
-   // setting up our webhook url on server spinup
+app.listen(PORT, () => {
    try {
     console.log(`Server is up and Running at PORT : ${PORT}`);
-    // await setupWebhook();
   } catch (error) {
     console.log(error.message);
   }
