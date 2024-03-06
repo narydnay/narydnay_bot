25598ca82a628caa45676fc3e32c2bd073b8a8c8
Alekseyua <alekseyuadnepr@gmail.com>
2024-03-06 23:18:07 +0200

    new vision 11

--- dist/api/telegram/index.js
+++ dist/api/telegram/index.js
@@ -1,6 +1,1 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const telegraf_1 = require("telegraf");
-const config_1 = require("../../config/config");
-const bot = new telegraf_1.Telegraf(config_1.config.get('token-bot'));
 //# sourceMappingURL=index.js.map
--- src/api/telegram/index.ts
+++
@@ -1,5 +0,0 @@
-import { Telegraf } from "telegraf";
-import { config } from "../../config/config";
-
-const bot = new Telegraf(config.get('token-bot'));
-
